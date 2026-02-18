7f413e1e552ec0550617f25c1b6ef38b7a98b620
Liam-Dowell <lcdowell10@gmail.com>
2026-02-18 16:10:37 -0600

    e

---
+++ Python/RSA/decode.py
@@ -0,0 +1,45 @@
+from sympy import randprime
+def rsa_decode():
+    i = 2
+    p = 0
+    q = 0
+    print("What is the n?")
+    n = int(input(":> "))
+    print("What is the e?")
+    e = int(input(":> "))
+    print("What is the ciphered text?")
+    c = int(input(":> "))
+
+    i = 2
+    limit = int(n**0.5) + 1
+
+    while i <= limit and n % i != 0:
+        i += 1
+
+    if i > limit:
+        print("n is not a product of two primes.")
+        return
+    p = i
+    q = int(n / i)
+
+    phi = (p - 1) * (q - 1)
+    d = pow(e, -1, phi)
+
+    private_key = (n, d)
+
+    m = decrypt(private_key, c)
+    byte_length = (m.bit_length() + 7) // 8
+    msg_bytes = m.to_bytes(byte_length, 'big')
+    print(m)
+    print(f"The message is {msg_bytes}.")
+
+def decrypt(private_key, c):
+    n, d = private_key
+    m = (c**d) % n
+    return m
+
+
+
+
+
+    
